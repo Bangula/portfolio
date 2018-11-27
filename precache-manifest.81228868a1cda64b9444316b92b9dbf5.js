0caa359f853b0357ed99b10776c875b91b6dde7c
Branko Vukovic <bvukovic082@gmail.com>
2018-11-27 15:18:39 +0100

    Updates

---
+++ precache-manifest.81228868a1cda64b9444316b92b9dbf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "0ff789a31c1b0dbc0d1a",
+    "url": "/portfolio/static/js/main.0ff789a3.chunk.js"
+  },
+  {
+    "revision": "a2c57e23517848f6845e",
+    "url": "/portfolio/static/js/1.a2c57e23.chunk.js"
+  },
+  {
+    "revision": "0ff789a31c1b0dbc0d1a",
+    "url": "/portfolio/static/css/main.faccd8cd.chunk.css"
+  },
+  {
+    "revision": "a2c57e23517848f6845e",
+    "url": "/portfolio/static/css/1.a55d224c.chunk.css"
+  },
+  {
+    "revision": "9c2c0322bf446cd043f42d1ae9da027a",
+    "url": "/portfolio/index.html"
+  }
+];
