c9efb9a88da11dc833b220cb3a48c49e23f2584d
Branko Vukovic <bvukovic082@gmail.com>
2018-11-30 17:48:19 +0100

    Updates

--- precache-manifest.62f39f7821b551b84b955feffc81a730.js
+++ precache-manifest.62f39f7821b551b84b955feffc81a730.js
@@ -4,16 +4,16 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/web-development2.1a389665.png"
   },
   {
-    "revision": "528c4742249bb68808c3",
-    "url": "/portfolio/static/css/main.cf6d166b.chunk.css"
+    "revision": "ab0c864087b6faaa5086",
+    "url": "/portfolio/static/css/main.856b7c98.chunk.css"
   },
   {
     "revision": "346699e4e9b6a1cf6acf3db16e3f3982",
     "url": "/portfolio/static/media/headlines.346699e4.png"
   },
   {
-    "revision": "e320806150749ee6bbf1",
-    "url": "/portfolio/static/js/1.e3208061.chunk.js"
+    "revision": "b457eae279f936d4ed43",
+    "url": "/portfolio/static/js/1.b457eae2.chunk.js"
   },
   {
     "revision": "ee2a100325fd284f0097",
@@ -24,8 +24,12 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/up-arrow.c094d1a6.png"
   },
   {
-    "revision": "528c4742249bb68808c3",
-    "url": "/portfolio/static/js/main.528c4742.chunk.js"
+    "revision": "5f63deee0da6d0f40b515778b2562202",
+    "url": "/portfolio/static/media/profile.5f63deee.jpg"
+  },
+  {
+    "revision": "ab0c864087b6faaa5086",
+    "url": "/portfolio/static/js/main.ab0c8640.chunk.js"
   },
   {
     "revision": "92af98659b02b98b5d352f083dc5d7af",
@@ -44,11 +48,11 @@ self.__precacheManifest = [
     "url": "/portfolio/static/media/coffee.1d20b672.png"
   },
   {
-    "revision": "e320806150749ee6bbf1",
-    "url": "/portfolio/static/css/1.62bdacee.chunk.css"
+    "revision": "b457eae279f936d4ed43",
+    "url": "/portfolio/static/css/1.b90c5d37.chunk.css"
   },
   {
-    "revision": "dd0aff840a2a023fd1450393f0e82cd4",
+    "revision": "14c4545dcbd4f9b783c8b999a0e29978",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.93c3d0ec30136ceb8b43ee96c952cc4d.js"
+  "/portfolio/precache-manifest.62f39f7821b551b84b955feffc81a730.js"
 );
 
 workbox.clientsClaim();
